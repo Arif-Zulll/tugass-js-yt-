02630bf5c3bdcc93c2d4bf65dfbf92e8fb40fb26
Afdehxyz <arifzulfikripardede@gmail.com>
2026-09-17 13:23:59 +0700

    bing bing

---
+++ script.js
@@ -0,0 +1,160 @@
+const YouTubeApp = {
+  categories: ["Semua", "Musik", "🔥Trending", "Podcast", "Live", "Animasi", "Komedi sketsa", "Musik Rap", "Memasak"],
+  
+  videos: [
+    { 
+      title: "PRESET AM 'GUE MEMANG POKE🤙🤙🤙'", 
+      channel: "AfDexx", 
+      views: "167 rb x ditonton", 
+      time: "3 Tahun yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/JCF-U7D5cB0/hqdefault.jpg",
+      avatar: "https://yt3.ggpht.com/R8Wf6brZlI0VIVZ7swLg31D3RqevKGv3iOL3fAH4EWohkvUdQA0gvWdZ2Y4IAO8ho97rs73P=s600-c-k-c0x00ffffff-no-rj-rp-mo" 
+    },
+    { 
+      title: "Heavy Lies The Crown (official music video)", 
+      channel: "510 Music", 
+      views: "82 rb x ditonton", 
+      time: "4 Hari yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/Pliq_68YV6s/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQczJeGkMNRsVfYOtRnaEuWNr6oMkZrMbBEOPn5f9iquQ&s=10"
+    },
+    { 
+      title: "Boboiboy takut rotan?!! #BoBoiBoyS3 | Episod 03", 
+      channel: "Monsta Universe", 
+      views: "11 jt x ditonton", 
+      time: "5 Tahun yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/3nVpttW53nM/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV-4llI3OcRgRkxGrv8T7iyorayO-JqdnQHaaM0jMPiA&s=10"
+    },
+    { 
+      title: "NYAMPURIN MIE TERPEDAS INDOMIE AYAM GEPREK DAN MIE SEDAAP", 
+      channel: "Tanboy Kun", 
+      views: "1,2 jt x ditonton", 
+      time: "4 Bulan yang lalu", 
+      thumbnail: "https://cdn0-production-images-kly.akamaized.net/2HOozjVrAhxZRL-RTxRlqybeGbE=/1280x720/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/5113320/original/056441000_1738220566-Mukbang_Tanboy_Kun_Santap_12_Bungkus_Mie_Sedaap_Varian_Baru_ala_Chef_Devina.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQznXkBq2sBMDRw518D_VuHdQaQ86DWzlJdqq-CF98pg&s=10"
+    },
+    { 
+      title: "HARI KEMERDEKAAN", 
+      channel: "Maell lee", 
+      views: "104 rb x ditonton", 
+      time: "1 Hari yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/BMkRKLTUMBA/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlvkubCGFv-GEl0sr8sBzkja9XmxkuQ0LDQeQh4dCpLA&s=10"
+    },
+    { 
+      title: "510-Plastic Faith (Visualizer)", 
+      channel: "510 Music", 
+      views: "34 rb x ditonton", 
+      time: "7 Bulan yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/W-wkWP9-2dc/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQczJeGkMNRsVfYOtRnaEuWNr6oMkZrMbBEOPn5f9iquQ&s=10"
+    },
+    { 
+      title: "ESA X PERIH", 
+      channel: "510 Music", 
+      views: "1,1 jt x ditonton", 
+      time: "3 Bulan yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/g1ShexE8ko8/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQczJeGkMNRsVfYOtRnaEuWNr6oMkZrMbBEOPn5f9iquQ&s=10"
+    },
+    { 
+      title: "TERBONGKAR PART 1 - Dhot Design", 
+      channel: "Dhot Design", 
+      views: "7,1 jt x ditonton", 
+      time: "1 Hari yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/6G-wmEsCfXg/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNMh7Kzx52h5umcK0Y_vBlAJwzllzVrT_CkUDySpgnrA&s=10"
+    },
+    { 
+      title: "Barasuara - Terbuang Dalam Waktu(official video)", 
+      channel: "Barasuara", 
+      views: "70 jt x ditonton", 
+      time: "3 Tahun yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/X-EK60rmcQs/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsfG3YGBSc4zg2JkZ03p-hG8Bkec1xdwKT1Zp7oeWaQQ&s"
+    }
+  ],
+
+  trendingVideos: [
+    { 
+      title: "Naykilla - MMG (My Mine Gueh) (Official Music Video)", 
+      channel: "antinrml dan NAYKILLA", 
+      views: "21 jt x ditonton", 
+      time: "2 Bulan yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/K-7olbaOelg/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMJfdKYQGokIIu5uzz-14huV36csmBseW-sK6gG426Tg&s=10"
+    },
+    { 
+      title: "Raim Laode-Dunia Yang Nanti (lyric video)", 
+      channel: "Raim Laode", 
+      views: "290 rb x ditonton", 
+      time: "2 hari yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/awWKxGftWh4/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRptCD61JhlDZphXa4n-rN5tHRxvtbu1XJhW1zLLa3eg&s"
+    },
+    { 
+      title: "enau feat. Ari Lesmana - Sesi Potret (Official Music Video)", 
+      channel: "Aku enau", 
+      views: "66 jt x ditonton", 
+      time: "6 Bulan yang lalu", 
+      thumbnail: "https://i.ytimg.com/vi/Nskf70DMR60/hq720.jpg",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKywEF2nJNxyvltsuwin_qx_V7uHsGe61u6Cu-nof8DQ&s"
+    }
+  ],
+
+  renderNavbar() {
+    return '<header class="navbar">' +
+      '<a href="#" class="yt-logo"></a>' +
+      '<div class="search-container">' +
+        '<input type="text" class="search-input" placeholder="Telusuri">' +
+        '<button class="search-btn">🔍</button>' +
+        '<button class="mic-btn">🎙️</button>' +
+      '</div>' +
+    '</header>';
+  },
+
+  renderCategories() {
+    const categoryChips = this.categories.map(function(cat, i) {
+      const activeClass = i === 0 ? "active" : "";
+      return '<div class="category-chip ' + activeClass + '">' + cat + '</div>';
+    }).join("");
+
+    return '<div class="categories">' + categoryChips + '</div>';
+  },
+
+  renderVideoCard(item) {
+    return '<div class="video-card">' +
+      '<div class="thumbnail" style="background-image: url(\'' + item.thumbnail + '\'); background-size: cover; background-position: center;"></div>' +
+      '<div class="video-info">' +
+        '<div class="avatar" style="background-image: url(\'' + item.avatar + '\'); background-size: cover; background-position: center; border-radius: 50%;"></div>' +
+        '<div class="details">' +
+          '<h4>' + item.title + '</h4>' +
+          '<p>' + item.channel + '</p>' +
+          '<p>' + item.views + ' • ' + item.time + '</p>' +
+        '</div>' +
+      '</div>' +
+    '</div>';
+  },
+
+  renderVideoGrid(videoList, title) {
+    if (!title) { title = ""; }
+    const titleHtml = title ? '<h2 class="section-title">' + title + '</h2>' : "";
+    const self = this;
+    
+    const cardsHtml = videoList.map(function(video) {
+      return self.renderVideoCard(video);
+    }).join("");
+
+    return titleHtml + '<main class="video-grid">' + cardsHtml + '</main>';
+  },
+
+  renderAll() {
+    return this.renderNavbar() +
+      this.renderCategories() +
+      this.renderVideoGrid(this.videos) +
+      this.renderVideoGrid(this.trendingVideos, "🔥 Trending");
+  }
+};
+
+document.write(YouTubeApp.renderAll());
